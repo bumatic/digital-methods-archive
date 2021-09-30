32652d82aeec80c25c83fdc5f0f6cff775642053
bumatic <marcus.burkhardt@gmail.com>
2021-09-30 08:43:56 +0200

    Update pull_updates.sh

--- pull_updates.sh
+++ pull_updates.sh
@@ -51,13 +51,16 @@ echo "Pull updates for bitchute-scraper"
 git subtree pull --prefix bitchute-scraper/source https://github.com/bumatic/bitchute-scraper.git main
 echo ""
 
-# echo "Pull updates for "
+# echo "Pull updates for rawgraphs-core"
+git subtree pull --prefix rawgraphs/rawgraphs-core/source https://github.com/rawgraphs/rawgraphs-core.git master
 # echo ""
 
-# echo "Pull updates for "
+# echo "Pull updates for rawgraphs-app"
+git subtree pull --prefix rawgraphs/rawgraphs-app/source https://github.com/rawgraphs/rawgraphs-app.git master
 # echo ""
 
-# echo "Pull updates for "
+# echo "Pull updates for rawgraphs-charts"
+git subtree pull --prefix rawgraphs/rawgraphs-charts/source https://github.com/rawgraphs/rawgraphs-charts.git master
 # echo ""
 
 # echo "Pull updates for "
